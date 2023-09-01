a16f0e9faab5505f539de67459ef18e1dd1efd33
Danisa <cuellodanisa.17@gmail.com>
2023-09-01 13:48:11 -0300

    primera subida

---
+++ DesafioPOO1Numero1-telefono/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace DesafioPOO1Numero1_telefono
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Telefono telefono1 = new Telefono("redmi","xiaomi");
+            //forma larga de cargar propiedades
+            //telefono1.setNumeroTelefonico("2622444192");
+            // Console.WriteLine("El numero de telefono es: " + telefono1.getNumeroTelefonico());
+            //forma corta de cargar propiedades
+            telefono1.NumeroTelefonico = "2622444192";
+             Console.WriteLine("El numero de telefono es: " + telefono1.NumeroTelefonico);
+            //metodo
+            
+            //Console.WriteLine(telefono1.llamar());
+            //sobrecarga de metodo
+            Console.WriteLine(telefono1.llamar("juan"));
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ DesafioPOO1Numero1-telefono/Telefono.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioPOO1Numero1_telefono
+{
+    internal class Telefono
+    {
+        //atributos
+        private string modelo;
+        private string marca;
+        private string numeroTelefonico;
+        private int codigoOperados;
+
+        //constructor
+
+        public Telefono (string modelo, string marca)
+        {
+            this.modelo = modelo;
+            this.marca = marca;
+        }
+
+        //propiedades
+
+        public string Modelo { get { return modelo; } }
+
+        public string Marca { get { return marca; } }
+
+        public string NumeroTelefonico
+        {
+            get { return numeroTelefonico; }
+            set { numeroTelefonico = value;}
+        }
+
+       // public void setNumeroTelefonico (string nT)  { numeroTelefonico = nT; }
+       // public string getNumeroTelefonico() { return numeroTelefonico; }
+
+        
+
+        public int  CodigoOperador
+        {
+            get { return codigoOperados;}
+            set
+            {
+                if (value > 0 && value <= 3 )
+                {
+                    codigoOperados = value;
+                }
+                else
+                {
+                    codigoOperados = 0;
+                }
+            }
+        }
+
+        //metodo
+        //public string llamar()
+       // {
+         //   return "Realizando llamada";
+
+       // }
+
+        //sobrecarga de metodo
+        public string llamar(string contacto)
+        {
+            return "realizando llamada" + contacto;
+        }
+
+    }
+
+}
